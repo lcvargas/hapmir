f91c683f6562a25f5cebc5b5412fb939ac0fa294
Dylan Sun <dylanfsun@gmail.com>
2014-07-07 13:12:10 -0400

    data is read once and saved to memory

--- Linkage/parser.pm
+++ Linkage/parser.pm
@@ -2,7 +2,7 @@ package Linkage::parser;
 use Moose; 
 
 has 'snp_location' => ( isa => 'Any', is => 'rw',  required => 1);
-has 'data' => ( isa => 'ArrayRef', is => 'rw', required => 0);
+has 'data' => ( isa => 'HashRef', is => 'rw',  required => 0);
 
 sub snpLength {
     my $self = shift;
@@ -13,12 +13,21 @@ sub snpLength {
     return length($first_line);
 }
 
-sub sampleSize {
+# parse() reads the data into memory and returns the number of lines in the file
+sub parse {
     my $self = shift;
+    my $counter = 0;
     open(DATA, $self->{"snp_location"}) || die "Can't open snp file: $!";
-    while (<DATA>) {};
-    return $.;
-
+    while (<DATA>) {
+        chomp;
+        $counter++;
+        ${$self->{"data"}}{$counter} = $_;
+    }
+    close DATA;
+    # foreach my $keys (keys %{$self->{"data"}}) {
+    #    print "$keys => ${$self->{'data'}}{$keys}\n";
+    # }
+    return $counter; 
 }
 
 # getRegion($starting_position, $window_size)
@@ -26,28 +35,21 @@ sub getRegion {
     my $self = shift;
     my $starting_position = shift;
     my $window_size = shift;
-    open(DATA, $self->{"snp_location"}) || die "can't open file: $!";
     my %uniques;
-    my $line;
-    while (<DATA>) {
-        chomp;
-        $line = substr($_, $starting_position, $window_size);
-        if ($line !~ m/(0|1)+/) { # only allow 0's and 1's 
+    my $region;
+    foreach my $line (keys %{$self->{"data"}}) {
+        $region = substr(${$self->{'data'}}{$line}, $starting_position, $window_size);
+        if ($region !~ m/(0|1)+/) { # only allow 0's and 1's 
             next;
         }
-        # throw out ?'d code here at this point; line must have 0 or 1 only; next if no match
-        if (exists $uniques{$line}) {
-            $uniques{$line}++;
+        if (exists $uniques{$region}) {
+            $uniques{$region}++;
         }
         else {
-            $uniques{$line} = 1;
+            $uniques{$region} = 1;
         }
     }
     close DATA;
-    # print "region 1:\n";
-    # foreach my $haplotype (keys %r1_uniques) {
-    #     print "$haplotype: $r1_uniques{$haplotype}\n";
-    # }
     return %uniques;
 }
 
@@ -59,29 +61,22 @@ sub setJointRegion {
     my $region_1_starting_position = shift;
     my $region_2_starting_position = shift;
     my $window_size = shift;
-    open(DATA, $self->{"snp_location"}) || die "can't open file: $!";
     my %unique_haplotypes;
-    my $line;
-    while (<DATA>) {
-        chomp;
-        $line = substr($_, $region_1_starting_position, $window_size) . 
-            substr($_, $region_2_starting_position, $window_size);
-        if ($line !~ m/(0|1)+/) { # only allow 0's and 1's 
+    my $region;
+    foreach my $line (keys %{$self->{"data"}}) {
+        $region = substr(${$self->{'data'}}{$line}, $region_1_starting_position, $window_size) . 
+            substr(${$self->{'data'}}{$line}, $region_2_starting_position, $window_size);
+        if ($region !~ m/(0|1)+/) { # only allow 0's and 1's 
             next;
         }
-        # throw out ?'d code here at this point; line must have 0 or 1 only; next if no match
-        if (exists $unique_haplotypes{$line}) {
-            $unique_haplotypes{$line}++;
+        if (exists $unique_haplotypes{$region}) {
+            $unique_haplotypes{$region}++;
         }
         else {
-            $unique_haplotypes{$line} = 1;
+            $unique_haplotypes{$region} = 1;
         }
     }
     close DATA;
-    # print "joint region:\n";
-    # foreach my $haplotype (keys %unique_haplotypes) {
-    #    print "$haplotype: $unique_haplotypes{$haplotype}\n";
-    # }
     return %unique_haplotypes;
 }
 
--- sliding_windows.pl
+++ sliding_windows.pl
@@ -1,39 +1,34 @@
-#!/opt/local/bin/perl -w 
-use strict;
-use Moose;
-use Getopt::Long;
+#!/usr/bin/env perl -w
+use strict; use Moose; use Getopt::Long;
 use Linkage::multiMI;
 use Linkage::parser;
 
-my $window_size;
-my $snp_location;
+my $window_size = 10;
+my $snp_location = "snp.in";
 my $outfile_location = "mir.out";
-die "Usage: ./sliding_windows.pl -w window_size -i infile.snp -o outfile.csv\n" if @ARGV != 6;
 GetOptions(
     "window|w=i" => \$window_size,
     "snp|i|infile=s" => \$snp_location,
     "outfile|o=s" => \$outfile_location
-) or die "Usage: -w window_size -i infile.snp -o outfile.csv";
+) or die "Usage: -w window_size -i snp.in -o mir.out";
+die "Usage: ./sliding_windows.pl -w window_size -i snp.in -o mir.out\n" if @ARGV != 0;
 
 my $parser = Linkage::parser->new(
     snp_location => $snp_location,
-    data => []
+    data => {}
 );
 
 my $data = Linkage::multiMI->new(
-    region_1 => {},
-    region_2 => {},
-    joint_haplotypes => {}
+    region_1 => {}, # hash containing haplotype => frequency for region 1
+    region_2 => {}, # hash containing haplotype => frequency for region 1
+    joint_haplotypes => {} # hash with haplotype => frequency for joint region
 );
 
 my $tot_region_length = $parser -> snpLength();
-# print "total region length: $tot_region_length \n";
-my %total_mir;
-my %count;
-my %total_error;
-my $sample_size = $parser -> sampleSize();
-open(TABFILE, ">mir_data.tab") || die "Can't open output file $!";
-print "Writing output to mir_data.tab...\n";
+my $sample_size = $parser -> parse(); # read data into memory and 
+                                    # return the number of lines in file
+# print "$sample_size\n";
+open(TABFILE, ">$outfile_location") || die "Can't open output file $!";
 print TABFILE "start_pos_1\tstart_pos_2\twindow_size\tmir\tsys_error\n";
 for (my $i = 0; $i < $tot_region_length + 1 - $window_size; $i+=$window_size) {
     $data -> {"region_1"} = {$parser -> getRegion($i, $window_size)};
@@ -42,35 +37,36 @@ for (my $i = 0; $i < $tot_region_length + 1 - $window_size; $i+=$window_size) {
         $data -> {"joint_haplotypes"} = 
             {$parser -> setJointRegion($i, $j, $window_size)};
         print TABFILE "$i\t$j\t$window_size\t", $data->mir(), "\t", $data->systematic_error($sample_size), "\n";
-        # produces mir_data.tab for heatmap
-        if (exists $total_mir{abs($i - $j)}) {
-            $total_mir{abs($i - $j)} += $data -> mir();
-            $total_error{abs($i - $j)} += $data ->     
-                systematic_error($sample_size);
-            $count{abs($i - $j)}++; 
+        # produces mir_data.tab 
+        # if (exists $total_mir{abs($i - $j)}) {
+        #    $total_mir{abs($i - $j)} += $data -> mir();
+        #    $total_error{abs($i - $j)} += $data ->     
+        #        systematic_error($sample_size);
+        #    $count{abs($i - $j)}++; 
             
-        }
-        else {
-            $total_mir{abs($i - $j)} = $data -> mir();
-            $total_error{abs($i - $j)} = $data -> 
-                systematic_error($sample_size);
-            $count{abs($i - $j)} = 1; 
-        }
+        # }
+        # else {
+        #    $total_mir{abs($i - $j)} = $data -> mir();
+        #     $total_error{abs($i - $j)} = $data -> 
+        #        systematic_error($sample_size);
+        #     $count{abs($i - $j)} = 1; 
+        # }
     }
     # print "Completed: $i / ", $tot_region_length + 1 - $window_size, "\n";
 }
 close TABFILE;
 
-open(RFILE, "+>>$outfile_location") || die "can't open output file $!";
-seek(RFILE, 0, 0);
-if (<RFILE> !~ m/^Window/) {
-    print RFILE "Window_size\tDistance\tMIR\tError\n"; 
-}
-seek(RFILE, 0, 2);
-for my $distance (keys %total_mir) {
-    print RFILE "$window_size\t$distance\t", $total_mir{$distance} /  $count{$distance}, "\t", $total_error{$distance} / $count{$distance}, "\n";
-}
-close RFILE;
+
+# open(RFILE, "+>>$outfile_location") || die "can't open output file $!";
+# seek(RFILE, 0, 0);
+# if (<RFILE> !~ m/^Window/) {
+#     print RFILE "Window_size\tDistance\tMIR\tError\n"; 
+# }
+# seek(RFILE, 0, 2);
+# for my $distance (keys %total_mir) {
+#    print RFILE "$window_size\t$distance\t", $total_mir{$distance} /  $count{$distance}, "\t", $total_error{$distance} / $count{$distance}, "\n";
+# }
+# close RFILE;
 
 
 exit;
